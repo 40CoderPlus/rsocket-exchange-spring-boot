29c538146ff4f45623f3ca7014ee64be728a73bc
Codingendless <codingendless@40coderplus.com>
2023-02-14 21:03:24 +0800

    rename sample application class name

--- rsocket-exchange-spring-boot-sample/src/main/java/com/fortycoderplus/rsocket/exchange/sample/RSocketExchangeSampleApplication.java
+++ rsocket-exchange-spring-boot-sample/src/main/java/com/fortycoderplus/rsocket/exchange/sample/RSocketExchangeSampleApplication.java
@@ -24,9 +24,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RSocketExchangeApplication {
+public class RSocketExchangeSampleApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(RSocketExchangeApplication.class, args);
+        SpringApplication.run(RSocketExchangeSampleApplication.class, args);
     }
 }
